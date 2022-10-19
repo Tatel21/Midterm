898062b9c15e7e7289b62cda2846e6ebc19d9c07
Tatel21 <102274074+Tatel21@users.noreply.github.com>
2022-10-20 00:56:36 +0800

    Add files via upload

---
+++ midterm/app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request,redirect
+from models import db, StudentModel
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_first_request
+def create_table():
+	db.create_all()
+
+@app.route('/create' ,methods = ['GET', 'POST'])
+def create():
+	if request.method == 'GET':
+		return render_template('create.html')
+
+app.run(host = 'localhost', port=5000)
